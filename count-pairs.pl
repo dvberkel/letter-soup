febdf9500d333c70a2a2b0e28c03c6e9872e0eba
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-07-19 10:06:02 +0200

    count occurance of pair once per word

--- count-pairs.pl
+++ count-pairs.pl
@@ -7,14 +7,19 @@ my %pairs = ();
 
 while (my $word = <>) {
     chomp($word);
+    my %local_pairs = ();
     for (my $i = 0; $i < length($word) - 1; $i++) {
 	my $first = substr $word, $i, 1;
 	for (my $j = $i + 1; $j < length($word); $j++) {
 	    my $second = substr $word, $j, 1;
 	    my $pair = $first . $second;
-	    my $count = $pairs{$pair} || 0;
-	    $pairs{$pair} = $count + 1;
+	    my $count = $local_pairs{$pair} || 0;
+	    $local_pairs{$pair} = $count + 1;
 	}
+    }
+    for(keys %local_pairs) {
+	my $count = $pairs{$_} || 0;
+	$pairs{$_} = $count + 1;
     }
 }
 
